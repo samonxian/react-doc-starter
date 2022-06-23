f49d0a175af01d06eac282bda2b726f80d056d07
samonxian <samonxian@tencent.com>
2022-06-23 16:19:22 +0800

    sdfsfdsf

---
+++ website/docusaurus.config.js
@@ -0,0 +1,132 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '业务组件文档',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'samonxian', // Usually your GitHub org/user name.
+  projectName: 'react-doc-starter', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/samonxian/react-doc-starter/tree/master/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/samonxian/react-doc-starter/tree/master/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '业务组件文档',
+        logo: {
+          alt: '组件文档 Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: '组件',
+          },
+          {to: '/blog', label: '博客', position: 'right'},
+          {
+            href: 'https://github.com/samonxian/react-doc-starter',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      // 页脚不需要可以不配置
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '学习',
+            items: [
+              {
+                label: '组件介绍',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: '社区',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: '更多',
+            items: [
+              {
+                label: '博客',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/samonxian/react-doc-starter',
+              },
+            ],
+          },
+        ],
+        copyright: `版权所有 © ${new Date().getFullYear()} 业务组件文档, Inc. 此网站使用 Docusaurus 构建。`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
