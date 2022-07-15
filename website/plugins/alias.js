e5614959f488af99c450ecc2305f84e22094f58c
samonxian <samonxian@tencent.com>
2022-07-15 11:08:42 +0800

    fix: 🐛 解决文档 alias 设置不正确的问题

--- website/plugins/alias.js
+++ website/plugins/alias.js
@@ -10,7 +10,7 @@ module.exports = function () {
             'react-antd-business-components/lib': path.resolve(__dirname, '../../packages/components/src'),
             'react-antd-business-components': path.resolve(__dirname, '../../packages/components/src'),
             'business-utils/lib': path.resolve(__dirname, '../../packages/utils/src'),
-            'business-utils': path.resolve(__dirname, '../../packages/utils'),
+            'business-utils': path.resolve(__dirname, '../../packages/utils/src'),
             $components: path.resolve(__dirname, '../../packages/components/src'), // 用于缩短文档路径
             $demo: path.resolve(__dirname, '../demo'), // 用于缩短文档路径
             $utils: path.resolve(__dirname, '../../packages/utils/src'), // 用于缩短文档路径
